9298dd0f128c9c12023ceb6fe455ba74ed1c94d1
voduyquoc <voduyquockt@gmail.com>
2024-06-10 08:53:09 +0200

    Week 3

--- 03_Week_3/mlops-mage/mlops/homework/data_exporters/register_model.py
+++ 03_Week_3/mlops-mage/mlops/homework/data_exporters/register_model.py
@@ -22,21 +22,24 @@ def export_data(data, *args, **kwargs):
     """
     # Specify your data exporting logic here
     dv, lr = data
+    os.makedirs("models", exist_ok=True)
+    with open("models/dict_vectorizer.b", "wb") as f_out:
+        pickle.dump(dv, f_out)
 
-    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_tracking_uri("http://mlflow:5000")
+
+    # Ensure there are no active runs
+    if mlflow.active_run():
+        mlflow.end_run()
+
     mlflow.set_experiment("homework_03")
 
-    with mlflow.start_run():
-        # Log the model
-        mlflow.sklearn.log_model(lr, "linear_regression_model")
-        os.makedirs('models', exist_ok=True)
-        # Save the DictVectorizer as an artifact
-        with open('models/linear_regression_model.bin', 'wb') as f_out:
-            pickle.dump(lr, f_out)
-        with open('models/dv.bin', 'wb') as f_out:
-            pickle.dump(dv, f_out)
-        
-        mlflow.log_artifact('models/dv.bin', artifact_path="artifacts")
+    with mlflow.start_run() as run:
+        # Log model
+        mlflow.sklearn.log_model(lr, "artifacts")
+        # Optionally log other artifacts
+        mlflow.log_artifact("models/dict_vectorizer.b", artifact_path="preprocessor")
+        print(f"Model logged with run_id: {run.info.run_id}")
 
     
 
